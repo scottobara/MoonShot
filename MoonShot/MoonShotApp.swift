41a18d3f22934c37a712537f2b778ca3037abc76
Scott Obara <scott@rubbscreations.com>
2021-01-13 07:02:10 +1100

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Scott Obara on 13/1/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
