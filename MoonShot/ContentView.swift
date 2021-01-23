08046cbfa7436815f3236685ac033e69bb10f9cd
Scott Obara <scott@rubbscreations.com>
2021-01-23 18:48:26 +1100

    NavigationLink()

--- MoonShot/ContentView.swift
+++ MoonShot/ContentView.swift
@@ -7,31 +7,17 @@
 
 import SwiftUI
 
-struct CustomText: View {
-    static var printCount: Int = 0
-    var text: String
 
-    var body: some View {
-        Text(text)
-    }
-    
-    init(_ text: String) {
-        print("Creating a new CustomText: \(CustomText.printCount)")
-        CustomText.printCount += 1
-        self.text = text
-    }
-}
 
 struct ContentView: View {
     var body: some View {
-        ScrollView(.vertical) {
-            LazyVStack(spacing: 10) {
-                ForEach(0..<100) {
-                    CustomText("Item \($0)")
-                        .font(.title)
+        NavigationView {
+            List(0..<100) { row in
+                NavigationLink(destination: Text("Detail \(row)")) {
+                    Text("Row \(row)")
                 }
             }
-            .frame(maxWidth: .infinity)
+            .navigationBarTitle("SwiftUI")
         }
     }
 }
