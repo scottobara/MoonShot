259e04742f5078f86d002967fc919a9c9c6e9ab8
Scott Obara <scott@rubbscreations.com>
2021-01-24 20:43:59 +1100

    Day 40, Project 8, part 2; Formatting the mission view

---
+++ MoonShot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Scott Obara on 24/1/21.
+//
+
+import SwiftUI
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
